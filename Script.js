81087624ca7a4ccfefd9dea4aeae72cdc25ee3fe
maxprofesional <movodoeva@gmail.com>
2025-07-26 11:06:13 +0600

    update js

--- Script.js
+++ Script.js
@@ -2,11 +2,29 @@ const text = document.querySelector("#text");
 const count = document.querySelector("#counter");
 const leftCounter = document.querySelector("#leftCounter");
 
-let maxCounter = 20;
+const maxCounter = 20;
 let currentCount = 0;
+count.textContent = currentCount;
+leftCounter.textContent = maxCounter;
+
 text.addEventListener("input", () => {
   currentCount = text.value.split("").filter((item) => item != " ").length;
-  maxCounter -= Number(currentCount);
+
+  if (currentCount > maxCounter) {
+    let result = "";
+    let nonSpaceCount = 0;
+    for (let item of text.value) {
+      if (item !== " ") {
+        if (nonSpaceCount >= maxCounter) {
+          break;
+        }
+        nonSpaceCount++;
+      }
+      result += item;
+    }
+    text.value = result;
+  }
+  currentCount = text.value.split("").filter((item) => item != " ").length;
   leftCounter.textContent = maxCounter - currentCount;
   count.textContent = currentCount;
 });
